5c44e6f0a974847c9efe9fab6f4a93d895dc7126
Vincent Fazio <vfazio@gmail.com>
2026-05-02 16:22:34 -0500

    req_uninstall: compress_for_rename: terminate search directories
    The `unchecked` search paths derived from the input paths were not
    terminated with os.path.sep but were compared against the wildcards that
    are, so unless the path was a subdirectory of a wildcard, it would not
    match.
    
    For example, root = /A/B/C/D would be skipped due to wildcard /A/B/C/
    but if root = /A/B/C it would fail to be considered a match.
    
    Signed-off-by: Vincent Fazio <vfazio@gmail.com>

--- src/pip/_internal/req/req_uninstall.py
+++ src/pip/_internal/req/req_uninstall.py
@@ -115,7 +115,7 @@ def compress_for_rename(paths: Iterable[str]) -> set[str]:
     """
     case_map = {os.path.normcase(p): p for p in paths}
     remaining = set(case_map)
-    unchecked = sorted({os.path.split(p)[0] for p in case_map.values()}, key=len)
+    unchecked = sorted({os.path.join(os.path.dirname(p), "") for p in case_map.values()}, key=len)
     wildcards: set[str] = set()
 
     def norm_join(*a: str) -> str:
@@ -134,7 +134,7 @@ def compress_for_rename(paths: Iterable[str]) -> set[str]:
         # for the directory.
         if not (all_files - remaining):
             remaining.difference_update(all_files)
-            wildcards.add(root + os.sep)
+            wildcards.add(root)
 
     return set(map(case_map.__getitem__, remaining)) | wildcards
 
